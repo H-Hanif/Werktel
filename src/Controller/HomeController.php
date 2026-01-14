751e8e87fa8fe890e3e1b44a3e7d18dd8005e134
H-Hanif <302732920@student.rocmondriaan.nl>
2026-01-14 11:07:55 +0100

    echte htacces oegevoegd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,34 @@ final class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/functies', name: 'app_functies')]
+    public function functies(): Response
+    {
+        return $this->render('home/functies.html.twig');
+    }
+
+    #[Route('/download', name: 'app_download')]
+    public function download(): Response
+    {
+        return $this->render('home/download.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact/index.html.twig');
+    }
+
+    #[Route('/privacy', name: 'app_privacy')]
+    public function privacy(): Response
+    {
+        return $this->render('home/privacy.html.twig');
+    }
+
+    #[Route('/algemene-voorwaarden', name: 'app_algemene_voorwaarden')]
+    public function voorwaarden(): Response
+    {
+        return $this->render('home/voorwaarden.html.twig');
+    }
 }
